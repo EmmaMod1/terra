8c572ce378c95a45857ab15ef27e78f280d4bf72
Emma Mod <comptesazaz@gmail.com>
2025-04-08 15:52:27 -0400

    ici2

--- main.tf
+++ main.tf
@@ -1,11 +1,12 @@
-resource "aws_launch_configuration" "example" {
-  image_id        = "ami-0fb653ca2d3203ac1"
-  instance_type   = "t2.micro"
-  security_groups = [aws_security_group.instance.id]
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
 
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p ${var.server_port} &
-              EOF
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
 }
