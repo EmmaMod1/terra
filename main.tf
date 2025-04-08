c7e4c605bcebd879d85c2aa622b0579de8072ee1
Emma Mod <comptesazaz@gmail.com>
2025-04-08 16:21:41 -0400

    bn1

--- main.tf
+++ main.tf
@@ -1,40 +1,43 @@
-resource "aws_autoscaling_group" "example" {
-  aws_launch_template = aws_launch_configuration.example.name
-  vpc_zone_identifier  = data.aws_subnets.default.ids
-
-  min_size = 2
-  max_size = 10
-
-  tag {
-    key                 = "Name"
-    value               = "terraform-asg-example"
-    propagate_at_launch = true
-  }
-}
-
-
-# Déclaration du provider AWS
+# Fournisseur AWS
 provider "aws" {
-  region = "us-east-1"  # Remplace par la région que tu utilises
+  region = "us-east-1"  # Change la région si besoin
 }
 
-# Déclaration de la ressource Launch Configuration
-resource "aws_launch_template" "example" {
-  name          = "example-launch-config"
-  image_id      = "ami-0c02fb55956c7d316"  # Remplace par une AMI valide
-  instance_type = "t2.micro"
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-# Déclaration de la source de données des subnets
+# VPC/Subnets (assure-toi que tu as bien des subnets taggés)
 data "aws_subnets" "default" {
   filter {
     name   = "tag:Name"
-    values = ["terraform-asg-sub-example"]
- # Remplace par tes tags ou critères spécifiques
+    values = ["terraform-asg-sub-example"]  # Remplace par tes vrais tags de subnet
   }
 }
 
+# Launch Template
+resource "aws_launch_template" "example" {
+  name_prefix   = "example-launch-template-"
+  image_id      = "ami-0c02fb55956c7d316"  # Amazon Linux 2 (valide en us-east-1)
+  instance_type = "t2.micro"
+}
+
+# Auto Scaling Group
+resource "aws_autoscaling_group" "example" {
+  desired_capacity     = 1
+  max_size             = 2
+  min_size             = 1
+  vpc_zone_identifier  = data.aws_subnets.default.ids
+
+  launch_template {
+    id      = aws_launch_template.example.id
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-instance"
+    propagate_at_launch = true
+  }
+
+  health_check_type         = "EC2"
+  health_check_grace_period = 300
+  force_delete              = true
+  wait_for_capacity_timeout = "0"
+}
